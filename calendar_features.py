b51d9e56d77bd8a1ff5777b9570413e1e4847b3e
maxtheaxe <max.perrello@gmail.com>
2020-01-25 21:45:57 -0500

    finished target_day function, collects all events for specific day

--- calendar_features.py
+++ calendar_features.py
@@ -29,24 +29,30 @@ def parse_time(time_string):
 # upgrade_calendar() given event list, converts all string dates to datetime objs, so it 
 # doesn't need to be done repeatedly; would've rather done calendar and date classes
 def upgrade_calendar(event_list):
-	for event in events:
-		# start = event['start'].get('dateTime', event['start'].get('date'))
-		print(event['start']['dateTime'])
-		event['start']['dateTime'] = parse_time(event['start']['dateTime'])
-		print(event['start']['dateTime'])
-		# end = event['end'].get('dateTime', event['end'].get('date'))
-
-# target_day() returns list of events for target day, given a list of days and target
-def target_day(event_list, wanted_day):
-	for event in events:
-		start = event['start'].get('dateTime', event['start'].get('date'))
-		end = event['end'].get('dateTime', event['end'].get('date'))
-		if a == a:
-			return
+	for event in event_list: # for each event in the list
+		# set starting time to datetime obj
+		event['start']['dateTime'] = parse_time(event['start']['dateTime']) # start time
+		# set ending time to datetime obj
+		event['end']['dateTime'] = parse_time(event['end']['dateTime']) # end time
+	return event_list # return "upgraded" event list
 
 # compare_day() compares the day of two datetime objects; returns boolean truth
 def compare_day(day_a, day_b):
 	return ( day_a.date() == day_b.date() ) # compares dates ONLY, not time
+
+# target_day() returns list of events for target day, given a list of days and target
+# default value for wanted_day is today
+def target_day(event_list, wanted_day = datetime.datetime.utcnow()):
+	refined_event_list = [] # final list for events
+	for event in event_list: # for each event in the list
+		start = event['start'].get('dateTime', event['start'].get('date')) # start time
+		end = event['end'].get('dateTime', event['end'].get('date')) # end time
+		if (compare_day(start, end)): # skip multi-day events
+			print ("Multi-day event, skipping")
+			continue # move to next event
+		if (compare_day(start, wanted_day)): # if the wanted day is the same as event
+			refined_event_list.append(event) # add current event to event list
+	return refined_event_list # return the new list of events (from only target day)
 
 # compare_calendars() given two lists of events, returns availability (time w/o overlap)
 def compare_calendars(schedule_a, schedule_b):
