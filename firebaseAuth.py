42dd932ace11f2bf261977cdb18a95fcde5b22dc
EdmundLuii <edmund.lui1998@gmail.com>
2020-01-25 20:40:37 -0500

    made function to push data into firebase

--- firebaseAuth.py
+++ firebaseAuth.py
@@ -23,22 +23,45 @@ firebaseConfig = {
     "storageBucket": "conuhacks2020-6dc7f.appspot.com",
     "messagingSenderId": "434422996113"
 }
-firebase = pyrebase.initialize_app(firebaseConfig)
-db = firebase.database()
 
-username = ""
-name = "" #alpha
-zendeskAppId = 0 #alphanumeric
-zendeskAppUId = 0 #alphanumeric
-skills = ''
-quizNumbers = ''
+def pushDB(username, name, zendeskAppId, zendeskAppUId, skills, quizNumbers, overlappedEvents):
+    """
+    ----------------------------------------
+    Inserts given data into firebase db
+    ---------------------------------------
+    :param username: string
+    :param name: string
+    :param zendeskAppId: string
+    :param zendeskAppUId: string
+    :param skills: list
+    :param quizNumbers: list
+    :param overlappedEvents: list
+    --------------------------------------
+    :return: result - boolean
+    """
+    firebase = pyrebase.initialize_app(firebaseConfig)
+    db = firebase.database()
 
-data = {
-    'username':username,
-    'name':name,
-    'zendeskAppId':zendeskAppUId,
-    'skills':'',
-    'quizNumbers':'',
-    'overlappedEvents':'',
-}
-db.push(data)
+    skillsFinal = ''
+    for skill in skills:
+        skillsFinal += skill
+
+    quizNumbersFinal = ''
+    for num in quizNumbers:
+        quizNumbersFinal += num
+
+    overlappedEventsFinal = ''
+    for event in overlappedEvents:
+        overlappedEventsFinal += event
+
+    data = {
+        'username':username,
+        'name':name,
+        'zendeskAppID':zendeskAppId,
+        'zendeskAppUId':zendeskAppUId,
+        'skills':skillsFinal,
+        'quizNumbers':quizNumbersFinal,
+        'overlappedEvents':overlappedEventsFinal,
+    }
+    result = db.push(data)
+    return result
